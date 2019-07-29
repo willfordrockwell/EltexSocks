d6d3b7f057305a611a33a82cab06a6730645fbb5
Willford <ermacprodac@gmail.com>
2019-07-29 16:51:58 +0700

    add working raw UDP in Internet level

--- Client.c
+++ Client.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char const *argv[]) //IP server, Port server, Port Client
 {
-    char Message[MSG_LEN + 28];
+    char Message[MSG_LEN + UDP_HDR_SIZE + IP_HDR_SIZE];
 
     char Server_IP[IP_LENGTH];
     char Port_Str[PORT_LENGTH];
@@ -12,12 +12,14 @@ int main(int argc, char const *argv[]) //IP server, Port server, Port Client
 
     int Socket;
 
+    int SetVal = 1;
+
     struct sockaddr_in Server_Addr;
     socklen_t Server_Addr_Len = sizeof(Server_Addr);
 
-    struct udphdr *Header;
+    struct udphdr *Udp_Header;
 
-    Header = (struct udphdr *)Message;
+    struct iphdr *Ip_Header;
 
     if (argc < 4) {
         Get_IP("127.0.0.1", Server_IP);
@@ -44,11 +46,6 @@ int main(int argc, char const *argv[]) //IP server, Port server, Port Client
 
     Port = htons(atoi(Port_Str));
 
-    //fill udp header
-    Header->uh_dport = Server_Port;
-    Header->uh_sport = Port;
-    Header->uh_sum = 0;
-
     Server_Addr.sin_family = AF_INET;
     inet_pton(AF_INET, Server_IP, &(Server_Addr.sin_addr));
     Server_Addr.sin_port = Server_Port;
@@ -62,18 +59,55 @@ int main(int argc, char const *argv[]) //IP server, Port server, Port Client
         exit(1);
     }
 
+    //set internet-layer
+    setsockopt(Socket, IPPROTO_IP, IP_HDRINCL, &SetVal, sizeof(SetVal));
+
     //Type a message to Server
     printf("Type message to server\n");
-    memset(Message + 8, 0, MSG_LEN);
+    memset(Message + UDP_HDR_SIZE + IP_HDR_SIZE, 0, MSG_LEN);
 
-    fgets(Message + 8, MSG_LEN, stdin);
-    *(Message + 8 + strlen(Message + 8)) = '\0';
+    fgets(Message + UDP_HDR_SIZE + IP_HDR_SIZE, MSG_LEN, stdin);
+    *(Message + UDP_HDR_SIZE + IP_HDR_SIZE + strlen(Message + UDP_HDR_SIZE
+                                                    + IP_HDR_SIZE)) = '\0';
 
-    Header->uh_ulen = htons(strlen(Message + 8) + 8);
+    //fill udp header
+    Udp_Header = (struct udphdr *)(Message + IP_HDR_SIZE);
+    Udp_Header->uh_dport = Server_Port;
+    Udp_Header->uh_sport = Port;
+    Udp_Header->uh_ulen = htons(strlen(Message + UDP_HDR_SIZE + IP_HDR_SIZE)
+                                + UDP_HDR_SIZE);
+    Udp_Header->uh_sum = 0;
 
-    if (sendto(Socket, Message, strlen(Message + 8) + 8, MSG_CONFIRM,
+    //fill ip header
+    Ip_Header = (struct iphdr *)Message;
+    Ip_Header->version = 4;
+    Ip_Header->ihl = 5;
+    Ip_Header->tos = 0;
+    Ip_Header->tot_len = 0;//kernel do it
+    Ip_Header->id = 0;
+    Ip_Header->frag_off = 0;
+    Ip_Header->ttl = 64;
+    Ip_Header->protocol = IPPROTO_UDP;
+    Ip_Header->check = 0;//kernel do it
+    Ip_Header->saddr = 0;//kernel do it
+    Ip_Header->daddr = Server_Addr.sin_addr.s_addr;
+
+    if (sendto(Socket, Message, strlen(Message + IP_HDR_SIZE + UDP_HDR_SIZE)
+               + IP_HDR_SIZE + UDP_HDR_SIZE, MSG_CONFIRM,
                (struct sockaddr *) &Server_Addr, Server_Addr_Len) > 0) {
-        printf("Send to server: %s\n", Message + 8);
+                    printf("IP starts: %x\nMessage starts: %x\n", Ip_Header,
+                           &Message[0]);
+                    printf("UDP starts: %x\nMessage + starts: %x\n", Udp_Header,
+                           &Message[IP_HDR_SIZE]);
+                    printf("Send to server IP header:\nver: %x\nihl: %x\n"
+                    "tos: %x\ntot_len: %x\nid: %x\nflagoff: %x\nttl: %x\n"
+                    "proto: %x\ncheck: %x\nsaddr: %x\ndaddr: %x\n",
+                    Ip_Header->version, Ip_Header->ihl, Ip_Header->tos,
+                    Ip_Header->tot_len, Ip_Header->id, Ip_Header->frag_off,
+                    Ip_Header->ttl, Ip_Header->protocol, Ip_Header->check,
+                    Ip_Header->saddr, Ip_Header->daddr);
+                    printf("Send to server message : %s\n", 
+                           Message + IP_HDR_SIZE + UDP_HDR_SIZE);
     }
     else {
         printf("Error on sendig message: %s\n", strerror(errno));
@@ -83,19 +117,25 @@ int main(int argc, char const *argv[]) //IP server, Port server, Port Client
     //start recieving
     while (1)
     {
-        memset(Message, 0, MSG_LEN + 28);
+        memset(Message, 0, MSG_LEN + UDP_HDR_SIZE + IP_HDR_SIZE);
         if (recvfrom(Socket, Message, MSG_LEN, MSG_WAITALL,
                  (struct sockaddr *) &Server_Addr, &Server_Addr_Len) == -1) {
             printf("Error on recieving message: %s\n", strerror(errno));
             exit(3);
         }
 
-        //reading udp header
-        Header = (struct udphdr *)(Message + 20);
+        //reading ip and udp headers
+        Ip_Header = (struct iphdr *)Message;
+        Udp_Header = (struct udphdr *)(Message + IP_HDR_SIZE);
         
-        if (Header->uh_dport == Port && Header->uh_sport == Server_Port) {
-            printf("Message founded!\n");
-            break;
+        if (Ip_Header->daddr == Server_Addr.sin_addr.s_addr) {
+            printf("Message to client's IP\n");
+            if (Udp_Header->uh_dport == Port &&
+                Udp_Header->uh_sport == Server_Port) {
+                    printf("Message to client's Port. Message found: %s\n",
+                           Message + IP_HDR_SIZE + UDP_HDR_SIZE);
+                    break;
+            }
         }
     }
     close(Socket);
--- server.h
+++ server.h
@@ -2,6 +2,7 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <arpa/inet.h>
 #include <string.h>
 #include <stdlib.h>
@@ -14,6 +15,8 @@
 #define MAX_CLIENTS 5
 #define MSG_LEN 72
 #define NO_FLAGS 0
+#define UDP_HDR_SIZE sizeof(struct udphdr)
+#define IP_HDR_SIZE sizeof(struct iphdr)
 
 void Get_Port(char *Auto_Port, char *Server_Port);
 void Get_IP(char* Auto_IP, char *Server_IP);
